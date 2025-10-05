1d7cbe8e950fc8132176d53803f6d73d07f8dfa5
Suman Gunsola <workwithsuman0@gmail.com>
2025-10-05 12:05:41 +0000

    new commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/callyAi.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- components/PopUpButton.tsx
+++ components/PopUpButton.tsx
@@ -15,7 +15,7 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { useRouter } from 'next/navigation'
+
 interface UserInfo {
     email: string;
     phone: string;
@@ -28,7 +28,6 @@ const PopUpButton = () => {
         email: "",
         phone: ""
     })
-    const router = useRouter();
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setuserinfo((prev) => ({
             ...prev,
